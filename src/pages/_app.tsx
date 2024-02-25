72dd3a3c69e2b1143391a44a5c00a12e18fb00c2
FReptar0 <20213tn137@utez.edu.mx>
2024-02-25 14:59:22 -0600

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/src/utils/theme'
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
+}
+
+export default MyApp;
