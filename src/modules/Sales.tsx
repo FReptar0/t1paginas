bc86305eedcc2681773ab2738c84129080505136
FReptar0 <20213tn137@utez.edu.mx>
2024-02-29 12:14:44 -0600

    Agrega gráficos de ventas utilizando react-chartjs-2 y chart.js

--- src/modules/Sales.tsx
+++ src/modules/Sales.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
-import LineChartModelProps from '../utils/LineChartModel';
-import LineComponent from '../components/Line';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Container, Typography } from '@mui/material';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: false,
+        },
+    },
+    tension: 0.3,
+};
+
+const labels = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Ventas en linea',
+            data: [500, 600, 700, 2000, 1200, 800, 1000, 1000, 1500, 1000, 1500, 2000],
+            borderColor: 'rgb(217, 58, 38)',
+            backgroundColor: 'rgba(217, 58, 38, 0.5)',
+        },
+        {
+            label: 'Ventas en punto de venta',
+            data: [480, 400, 1000, 1100, 600, 600, 1000, 2100, 800, 500, 800, 1000],
+            borderColor: 'rgb(254, 193, 193)',
+            backgroundColor: 'rgba(254, 193, 193, 0.5)',
+        },
+    ],
+};
 const Sales: React.FC = () => {
 
     const xAxis = {
@@ -26,9 +76,20 @@ const Sales: React.FC = () => {
                 Ventas
             </Typography>
 
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            {/*<LineComponent xAxis={xAxis} series={series} width={width} height={height} />*/}
 
-            </div>
+            <Container
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <Line
+                    options={options}
+                    data={data}
+                />
+            </Container>
         </div>
     );
 };
